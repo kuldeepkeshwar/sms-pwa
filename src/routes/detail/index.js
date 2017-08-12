e6d7b762b78149c991fdf6a4907afa2050ac147a
anotherjsguy <kuldeepkeshwar@gmail.com>
2017-08-12 23:52:33 +0530

    added list & detail (#3)

---
+++ src/routes/detail/index.js
@@ -0,0 +1,44 @@
+import { h, Component } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style';
+import { getMessageThread } from './../../services/message';
+
+const Sender = (props) =>(<div>
+	<div><Link href="/"> Back </Link></div>
+	<div>
+		<div>{props.sender.name}</div>
+		<div>{props.sender.number}</div>
+	</div>
+</div>)
+const Messages = (props) =>(
+	<div>
+		{props.messages.map(msg=>{
+			return (
+				<div>
+					<div>{msg.time}</div>
+					<div>{msg.detail}</div>
+				</div>
+			)
+		})}
+	</div>
+)
+export default class Detail extends Component {
+	constructor(){
+		super();
+		this.state={
+			sender:{},
+			messages:[]
+		}
+	}
+	componentDidMount(){
+		getMessageThread().then((state)=>this.setState(state));
+	}
+	render() {
+		return (
+			<div>
+				<Sender sender={this.state.sender}/>
+				<Messages messages={this.state.messages} />
+			</div>
+		);
+	}
+}
