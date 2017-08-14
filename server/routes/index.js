b3488f30a4b3f552414699041f00b5733a0913a5
anotherjsguy <kuldeepkeshwar@gmail.com>
2017-08-14 22:25:10 +0530

    added scrollable (#9)

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,65 +5,34 @@ const router = express.Router();
 
 const appState = {
   total: 1000,
-  messages: [{
-    id: 101,
-    sender: {
-      number: 9004925450,
-      name: 'Mr. X',
-    },
-    time: '10/10/2017',
-    detail: 'Hi, Mr. X',
-  }, {
-    id: 102,
-    sender: {
-      number: 9004925450,
-      name: 'Mr. X',
-    },
-    time: '10/10/2017',
-    detail: 'Hi, Mr. X',
-  }, {
-    id: 103,
-    sender: {
-      number: 9004925450,
-      name: 'Mr. X',
-    },
-    time: '10/10/2017',
-    detail: 'Hi, Mr. X',
-  }, {
-    id: 104,
-    sender: {
-      number: 9004925450,
-      name: 'Mr. X',
-    },
-    time: '10/10/2017',
-    detail: 'Hi, Mr. X',
-  }],
+  messages: [],
 };
+for(let i=1;i<=50;i++){
+  appState.messages.push({
+    id: 100+i,
+    sender: {
+      number: 9004925450,
+      name: 'Mr. X'+i,
+    },
+    time: '10/10/2017',
+    detail: 'Hi, Mr. X'+i,
+  })
+}
 const messageThread = {
   total: 1000,
   sender: {
     number: 9004925450,
     name: 'Mr. X',
   },
-  messages: [{
-    id: 101,
-    time: '10/10/2017',
-    detail: 'Hi, Mr. X',
-  }, {
-    id: 102,
-    time: '10/10/2017',
-    detail: 'Hi, Mr. X',
-  }, {
-    id: 103,
-    time: '10/10/2017',
-    detail: 'Hi, Mr. X',
-  }, {
-    id: 104,
-    time: '10/10/2017',
-    detail: 'Hi, Mr. X',
-  }],
+  messages: [],
 };
-
+for(let i=1;i<=25;i++){
+  messageThread.messages.push({
+    id: 100+i,
+    time: '10/10/2017',
+    detail: 'Hi, Mr. X'+i,
+  })
+}
 router.get('/messages/', (req, res) => {
   res.send(appState);
 });
