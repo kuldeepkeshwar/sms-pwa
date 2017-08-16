04e604fd40d635cbbef1cd8e874224fc4d58110b
Nitish Tyagi <nitish@197NODNB16652.local>
2017-08-16 12:35:24 +0530

    Design

--- src/routes/list/index.js
+++ src/routes/list/index.js
@@ -1,42 +1,46 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
-import style from './style';
+import './style';
 import { getList } from './../../services/message';
 
-const SearchBox = props => (<div>
-  <input type="text" placeholder="1363 messages" />
-</div>);
-const Messages = props => (<div>
-  {props.messages.map((msg) => {
-    return (
-      <Link href={`/detail/${msg.id}`}>
-        <div><img src="/images/user" /></div>
-        <div>
-          <div>{msg.sender.name || msg.sender.number }</div>
-          <div>{msg.detail}</div>
-        </div>
-        <div>{msg.time}</div>
-      </Link>
-    );
-  })}
-</div>);
+const SearchBox = (props) => (<div className="search--bar--container">
+	<input className="search--bar" type="text" placeholder="1363 messages" />
+</div>)
+const Messages = (props) => (<div className="item--overflow">
+	{props.messages.map((msg)=>{
+		return (
+			<Link href={'/detail/'+msg.id} className="item--listing--link">
+				<div className="item--listing--container">
+					<div className="avatar--container"><img src="../assets/icons/android-chrome-192x192.png"/></div>
+					<div className="item--listing">
+						<div className="item--name--number">{msg.sender.name || msg.sender.number }</div>
+						<div className="item--detail">{msg.detail}</div>
+						<div className="item--date--time">{msg.time}</div>
+					</div>
+				</div>
+			</Link>
+		)
+	})}
+</div>)
 export default class List extends Component {
-  constructor() {
-    super();
-    this.state = {
-      messages: [],
-    };
-  }
-  componentDidMount() {
-    getList().then(state => this.setState(state));
-  }
-  render() {
-    return (
-      <div>
-        <h2>Messaging</h2>
-        <SearchBox />
-        <Messages messages={this.state.messages} />
-      </div>
-    );
-  }
+	constructor(){
+		super();
+		this.state={
+			messages:[]
+		}
+	}
+	componentDidMount(){
+		getList().then((state)=>this.setState(state));
+	}
+	render() {
+		return (
+			<div class="main--screen">
+				<h5 className="main--title">Messaging</h5>
+				<div className="main--inner--section">
+					<SearchBox />
+					<Messages messages={this.state.messages} />
+				</div>
+			</div>
+		);
+	}
 }
